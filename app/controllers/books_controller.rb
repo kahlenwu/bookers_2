3397bc069db09f0cc6fe4e95e0cc7a4f3650463f
kahlenwu <w2321698@gmail.com>
2021-10-14 09:16:43 +0000

    [Add]user_info

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,26 @@
 class BooksController < ApplicationController
   def index
+  @books = Book.all
+  @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def update
@@ -16,4 +28,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def show
   end
-  
+
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
   end
-  
+
 end
