17aade56ab555f9ac678d8615812c9fd469b0fdd
kahlenwu <w2321698@gmail.com>
2021-10-15 03:57:56 +0000

    [Add]user_edit page

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ec214f300138f95c6d7bece05a79c8fd86891566bcf81cdd0877365e108db6caf3e9c43b3d2e8053fbc093849b57c1151e293c4448a7899d924a7a5a3bdaf370'
