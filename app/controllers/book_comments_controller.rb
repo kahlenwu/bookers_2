9761fc0d14f5fd17cdda5f68ceb56a0c0273af84
kahlenwu <w2321698@gmail.com>
2021-10-21 10:40:44 +0000

    [Add]comment_error

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comments_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    if @book_comment.save
+      redirect_to book_path(@book.id)
+    else
+      render 'books/show'
+    end
   end
 
   def destroy
@@ -14,7 +18,7 @@ class BookCommentsController < ApplicationController
 
   private
 
-  def book_comments_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 
