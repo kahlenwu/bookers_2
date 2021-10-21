560b2ed01b0e9530144722447249cf95ae787a57
kahlenwu <w2321698@gmail.com>
2021-10-21 05:17:52 +0000

    [Add]book_comments routing, controller, association

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
