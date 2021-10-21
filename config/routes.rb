28e43e7dab0ac455d00af9cec11cbe0d951139bb
kahlenwu <w2321698@gmail.com>
2021-10-21 08:44:05 +0000

    [Add]favarites_model, routing, controller, association

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
 end
