1e4a7cc652836a85c75ba509707765daa36715b7
kahlen <w2321698@gmail.com>
2021-11-11 07:59:15 +0000

    [Add]検索機能

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,2 +1,44 @@
 class SearchsController < ApplicationController
+  def search
+    # viewのfrom_tagにて
+    # 選択したmodelの値を@modelに代入。
+    @model = params['model']
+    # 選択した検索方法の値を@methodに代入。
+    @method = params['method']
+    # 検索ワードを@contentに代入。
+    @content = params['content']
+    # @model, @content, @methodを代入した、search_forを@recordsに代入
+    @records = search_for(@model, @content, @method)
+  end
+
+    private
+    def search_for(model, content, method)
+      # 選択したモデルがuserだったら
+      if model == 'user'
+        # 選択した検索方法が完全一致だったら
+        if method == 'perfect'
+          User.where(name: content)
+          # 選択した検索方法が部分一致だったら
+        elsif method == 'partial'
+          User.where('name LIKE ?', '%'+content+'%')
+          # 選択した検索方法が前方一致だったら
+        elsif method == 'forward'
+          User.where('name LIKE ?', content+'%')
+          # 選択した検索方法が後方一致だったら
+        else
+          User.where('name LIKE ?', '%'+content)
+        end
+        # 選択したモデルがbookだったら
+      elsif model == 'book'
+        if method == 'perfect'
+          Book.where(title: content)
+        elsif method == 'partial'
+          Book.where('title LIKE ?', '%'+content+'%')
+        elsif method == 'forward'
+          Book.where('title LIKE ?', content+'%')
+        else
+          Book.where('title LIKE ?', '%'+content)
+        end
+      end
+    end
 end
